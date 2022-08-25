1029e05b121db546fb7fb9c60f7da6b413397cbe
Zelnatal <elnatan.honorato@gmail.com>
2022-08-25 20:41:04 -0300

    a

---
+++ js/head.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    var elementBody = document.querySelector('body');
+    var elementBtnIncreaseFont = document.getElementById('increase-font');
+    var elementBtnDecreaseFont = document.getElementById('decrease-font');
+    // Padrão de tamanho, equivale a 100% do valor definido no Body
+    var fontSize = 100;
+    var backgroundSize = 108;
+    // Valor de incremento ou decremento, equivale a 10% do valor do Body
+    var increaseDecrease = 10;
+
+    // Evento de click para aumentar a fonte
+    elementBtnIncreaseFont.addEventListener('click', function(event) {
+        fontSize = fontSize + increaseDecrease;
+        backgroundSize = backgroundSize + increaseDecrease;
+        elementBody.style.fontSize = fontSize + '%';
+        elementBody.style.backgroundSize = backgroundSize + '%';
+    });
+
+    // Evento de click para diminuir a fonte
+    elementBtnDecreaseFont.addEventListener('click', function(event) {
+        fontSize = fontSize - increaseDecrease;
+        backgroundSize = backgroundSize - increaseDecrease;
+        elementBody.style.fontSize = fontSize + '%';
+        elementBody.style.backgroundSize = backgroundSize + '%';
+    });
+}
+
+
